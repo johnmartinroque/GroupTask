171b4a661749084c29767c17c3d439cce55ed55b
johnmartinroque <johnmartinroque@gmail.com>
2025-06-25 21:56:18 +0800

    browserRouter

--- main/src/screens/Home.jsx
+++ main/src/screens/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TaskCard from "../components/TaskCard";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <TaskCard />
+    </div>
+  );
 }
 
 export default Home;
