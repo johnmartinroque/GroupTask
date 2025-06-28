496d6c226f0defdb70934673eeb8ee32bfdf322c
johnmartinroque <johnmartinroque@gmail.com>
2025-06-28 22:00:05 +0800

    sign in with google

--- main/src/screens/SignIn.jsx
+++ main/src/screens/SignIn.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { auth, googleProvider } from "../firebase";
 
 function SignIn() {
+  const register = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log("User signed in:", user);
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData?.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.error("Sign-in error:", errorCode, errorMessage);
+    }
+  };
+
   return (
     <div>
       <Row>
-        <Col>SignIn</Col>
+        <Col>
+          <button onClick={register}>Register</button>
+        </Col>
       </Row>
     </div>
   );
