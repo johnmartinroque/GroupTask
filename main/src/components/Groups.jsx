4cbf464d33ae2b7434a31d85fce090de0ec3c5c1
johnmartinroque <johnmartinroque@gmail.com>
2025-07-03 18:31:27 +0800

    create group and get groups

--- main/src/components/Groups.jsx
+++ main/src/components/Groups.jsx
@@ -1,33 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Groups() {
   const [newGroupName, setNewGroupName] = useState("");
   const [groupName, setGroupName] = useState("");
+  const [groupList, setGroupList] = useState([]);
 
   const groupCollectionRef = collection(db, "groups");
 
   const createGroup = async () => {
     await addDoc(groupCollectionRef, { groupName: newGroupName });
+    getGroupList();
+  };
+
+  const getGroupList = async () => {
+    const data = await getDocs(groupCollectionRef);
+    const filteredData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setGroupList(filteredData);
   };
 
   const joinGroupHandler = () => {
     console.log(groupName);
   };
 
+  useEffect(() => {
+    getGroupList();
+  }, []);
+
   return (
     <div>
       <Container>
         <Row>
-          <Col>hello</Col>
+          <Col>Groups</Col>
+          {groupList.map((group) => (
+            <Col key={group.id}>{group.groupName}</Col>
+          ))}
         </Row>
         <Row>
           <Col>
-            <Button>Create Group</Button>
+            <Button onClick={createGroup}>Create Group</Button>
             <input
               placeholder="Input new group name"
               value={newGroupName}
@@ -35,6 +53,7 @@ function Groups() {
             />
           </Col>
         </Row>
+
         <Row>
           <Col>
             <Button onClick={joinGroupHandler}>Join</Button>
