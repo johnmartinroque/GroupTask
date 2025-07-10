4727018c7adf67d64c8a5e4abf985d2d7c3a3814
johnmartinroque <johnmartinroque@gmail.com>
2025-07-10 21:51:47 +0800

    change password

--- main/src/screens/Profile.jsx
+++ main/src/screens/Profile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { onAuthStateChanged } from "firebase/auth";
+import ChangePassword from "../components/authentication/ChangePassword";
+
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      Profile
+      <ChangePassword />
+    </div>
+  );
 }
 
 export default Profile;
