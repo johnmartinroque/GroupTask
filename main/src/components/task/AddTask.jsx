6bd52445eb787c5ca1b14bc8074207d51cc6db13
johnmartinroque <johnmartinroque@gmail.com>
2025-07-23 16:31:37 +0800

    addTask component bootstrap

--- main/src/components/task/AddTask.jsx
+++ main/src/components/task/AddTask.jsx
@@ -1,7 +1,7 @@
 import { addDoc, collection } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { auth, db } from "../../firebase";
-import { Alert, Button, Col, Row, Spinner } from "react-bootstrap";
+import { Alert, Button, Col, Container, Row, Spinner } from "react-bootstrap";
 import { onAuthStateChanged } from "firebase/auth";
 
 function AddTask(props) {
@@ -91,45 +91,52 @@ function AddTask(props) {
   }, []);
 
   return (
-    <div>
-      <Row>
-        <Col>
-          {isLoading && (
-            <div className="spinner-border text-primary" role="status">
-              <span className="visually-hidden">
-                <Spinner />
-              </span>
-            </div>
-          )}
-          {!user ? (
-            <Alert variant="warning">Please log in to add tasks.</Alert>
-          ) : (
-            <>
-              <input
-                type="text"
-                placeholder="Task Name"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-              <textarea
-                type="text"
-                placeholder="Task Description"
-                value={newDescription}
-                onChange={(e) => setNewDescription(e.target.value)}
-                rows={4}
-                cols={80}
-              />
-              <label>Optional Deadline</label>
-              <input
-                type="datetime-local"
-                value={newDeadline}
-                onChange={(e) => setNewDeadline(e.target.value)}
-              />
-              <Button onClick={addTask}>SUBMIT</Button>
-            </>
-          )}
-        </Col>
-      </Row>
+    <div
+      className="d-flex align-items-center justify-content-center bg-light"
+      style={{ minHeight: "20vh", minWidth: "5vh" }}
+    >
+      <Container>
+        {isLoading && (
+          <div
+            className="p-5 rounded d-flex justify-content-center mb-3"
+            role="status"
+          >
+            <span className="visually-hidden">
+              <Spinner />
+            </span>
+          </div>
+        )}
+        {!user ? (
+          <Alert variant="warning">Please log in to add tasks.</Alert>
+        ) : (
+          <>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Task Name"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+            <textarea
+              type="text"
+              placeholder="Task Description"
+              className="form-control mb-2"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              rows={4}
+              cols={80}
+            />
+            <label>Optional Deadline</label>
+            <input
+              type="datetime-local"
+              className="form-control mb-3"
+              value={newDeadline}
+              onChange={(e) => setNewDeadline(e.target.value)}
+            />
+            <Button onClick={addTask}>SUBMIT</Button>
+          </>
+        )}
+      </Container>
     </div>
   );
 }
